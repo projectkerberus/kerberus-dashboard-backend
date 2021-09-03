57db36754f5c00e9445f271f2e981d372b15bdf8
mauro sala <mauro.sala@gmail.com>
2021-09-03 09:10:15 +0200

    feat(catalog): azure ad processor

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -1,12 +1,22 @@
 import {
   CatalogBuilder,
-  createRouter
+  createRouter,
 } from '@backstage/plugin-catalog-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
+import { MicrosoftGraphOrgReaderProcessor } from '@backstage/plugin-catalog-backend-module-msgraph';
 
-export default async function createPlugin(env: PluginEnvironment): Promise<Router> {
+export default async function createPlugin(
+  env: PluginEnvironment,
+): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
+
+  builder.addProcessor(
+    MicrosoftGraphOrgReaderProcessor.fromConfig(env.config, {
+      logger: env.logger,
+    }),
+  );
+
   const {
     entitiesCatalog,
     locationsCatalog,
